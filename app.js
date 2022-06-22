da81ec5eb9d7a6a32172399caef8faf68e9d0184
Audrey <audreyhershenson@gmail.com>
2022-06-22 17:45:11 -0500

    Added WIP  AI for computer player

--- app.js
+++ app.js
@@ -14,9 +14,10 @@
 
 const gameArea = document.getElementById('board');
 
-let player = "Player 1";
+let player = document.getElementById("p1name").value;
 let playerSymbol = "O";
-let gameRunning = true;
+let gameRunning = false;
+let opponent = null;
 
 let board = [
     [null, null, null],
@@ -24,9 +25,16 @@ let board = [
     [null, null, null]
 ]
 
+if  (document.getElementById("p2name").value === "Computer") {
+    opponent = "Computer";
+    
+}
+
 ///////////////////
 // interactivity //
 ///////////////////
+
+gameArea.add
 
 gameArea.addEventListener('click', function(clickEvent) {
     if (gameRunning === true) {
@@ -35,45 +43,36 @@ gameArea.addEventListener('click', function(clickEvent) {
                 playerSwitch();
                 clickEvent.target.textContent = playerSymbol;
                 arrayConvert(playerSymbol);
-                console.log(player + "'s turn!");
+                document.getElementById('message').innerHTML = (player + "'s turn!");
             }
         }  
         function arrayConvert(symbol) {
             if (clickEvent.target.matches('#A1')) {
                 board[0][0] = symbol;
-                console.log(board);
             }
             if (clickEvent.target.matches('#A2')) {
                 board[0][1] = symbol;
-                console.log(board);
             }
             if (clickEvent.target.matches('#A3')) {
                 board[0][2] = symbol;
-                console.log(board);
             }
             if (clickEvent.target.matches('#B1')) {
                 board[1][0] = symbol;
-                console.log(board);
             }
             if (clickEvent.target.matches('#B2')) {
                 board[1][1] = symbol;
-                console.log(board);
             }
             if (clickEvent.target.matches('#B3')) {
                 board[1][2] = symbol;
-                console.log(board);
             }
             if (clickEvent.target.matches('#C1')) {
                 board[2][0] = symbol;
-                console.log(board);
             }
             if (clickEvent.target.matches('#C2')) {
                 board[2][1] = symbol;
-                console.log(board);
             }
             if (clickEvent.target.matches('#C3')) {
                 board[2][2] = symbol;
-                console.log(board);
             }
         }
         checkWin(board);
@@ -86,12 +85,20 @@ gameArea.addEventListener('click', function(clickEvent) {
 ////////////////////////
 
 function playerSwitch() {
-    if (player === "Player 1") {
-        player = "Player 2";
-        playerSymbol = "O";
+    if (player === document.getElementById("p1name").value) {
+        
+        if (opponent === "Computer") {
+            console.log("Computer turn");
+            computerChoose();
+            
+        }
+        else {
+            player = document.getElementById("p2name").value;
+            playerSymbol = "O";
+        }
     }
-    else if (player === "Player 2") {
-        player = "Player 1";
+    else if (player === document.getElementById("p2name").value) {
+        player = document.getElementById("p1name").value;
         playerSymbol = "X";
     }
 }
@@ -101,45 +108,121 @@ function checkWin(board) {
     playerVic = null;
  
     if (playerSymbol === "X") {
-        playerVic = "Player 2";
+        playerVic = document.getElementById("p2name").value;
     }
     else if (playerSymbol === "O") {
-       playerVic = "Player 1";
+        playerVic = document.getElementById("p1name").value;
     }
 
     //horizontal
     if (board[0][0] === board [0][1] && board[0][1] === board[0][2] && board[0][0] !== null) {
-        console.log(playerVic + " WINS!");
+        document.getElementById('message').innerHTML = (playerVic + " WINS!");
+        document.getElementById("restart").style.display = "flex";
+        document.getElementById("restartButton").style.display = "block";
         gameRunning = false;
     }
     else if (board[1][0] === board [1][1] && board[1][1] === board[1][2] && board[1][0] !== null) {
-        console.log(playerVic + " WINS!");
+        document.getElementById('message').innerHTML = (playerVic + " WINS!");
+        document.getElementById("restart").style.display = "flex";
+        document.getElementById("restartButton").style.display = "block";
         gameRunning = false;
     }
     else if (board[2][0] === board [2][1] && board[2][1] === board[2][2] && board[2][0] !== null) {
-        console.log(playerVic + " WINS!");
+        document.getElementById('message').innerHTML = (playerVic + " WINS!");
+        document.getElementById("restart").style.display = "flex";
+        document.getElementById("restartButton").style.display = "block";
         gameRunning = false;
     }
     //vertical
     else if (board[0][0] === board [1][0] && board[0][0] === board[2][0] && board[0][0] !== null) {
-        console.log(playerVic + " WINS!");
+        document.getElementById('message').innerHTML = (playerVic + " WINS!");
+        document.getElementById("restart").style.display = "flex";
+        document.getElementById("restartButton").style.display = "block";
         gameRunning = false;
     }
     else if (board[0][1] === board [1][1] && board[0][1] === board[2][1] && board[0][1] !== null) {
-        console.log(playerVic + " WINS!");
+        document.getElementById('message').innerHTML = (playerVic + " WINS!");
+        document.getElementById("restart").style.display = "flex";
+        document.getElementById("restartButton").style.display = "block";
         gameRunning = false;
     }
     else if (board[0][2] === board [1][2] && board[0][2] === board[2][2] && board[0][2] !== null) {
-        console.log(playerVic + " WINS!");
+        document.getElementById('message').innerHTML = (playerVic + " WINS!");
+        document.getElementById("restart").style.display = "flex";
+        document.getElementById("restartButton").style.display = "block";
         gameRunning = false;
     }
     //diagonal
     else if (board[0][0] === board [1][1] && board[0][0] === board[2][2] && board[0][0] !== null) {
-        console.log(playerVic + " WINS!");
+        document.getElementById('message').innerHTML = (playerVic + " WINS!");
+        document.getElementById("restart").style.display = "flex";
+        document.getElementById("restartButton").style.display = "block";
         gameRunning = false;
     }
     else if (board[0][2] === board [1][1] && board[0][2] === board[2][0] && board[0][2] !== null) {
-        console.log(playerVic + " WINS!");
+        document.getElementById('message').innerHTML = (playerVic + " WINS!");
+        document.getElementById("restart").style.display = "flex";
+        document.getElementById("restartButton").style.display = "block";
         gameRunning = false;
     }
+}
+
+function computerChoose() {
+
+    arrayChoose = ["A1","A2","A3","B1","B2","B3","C1","C2","C3"];
+    let computerMove = Math.floor(Math.random() * 8);
+    console.log(computerMove);
+    let computerChoice = arrayChoose[computerMove];
+    let space = document.getElementById(computerChoice); 
+    if (space.textContent === "") {
+        space.textContent = "X";
+    }
+    else if (space.textContent !== "") {
+        computerChoose();
+    }
+}
+
+function startGame() {  
+    gameRunning = true;
+
+    let divStarting = document.getElementById("startingScreen");
+    let divGameplay = document.getElementById("gameplayScreen");
+    divStarting.style.display = "none";
+    divGameplay.style.display = "flex";
+
+    document.getElementById('player1').innerHTML =  document.getElementById("p1name").value;
+    document.getElementById('player2').innerHTML =  document.getElementById("p2name").value;
+}
+
+function restartGame() {
+    player = document.getElementById("p1name").value;
+    playerSymbol = "O";
+    gameRunning = false;
+
+    board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+    ]
+    document.getElementById('message').innerHTML = ("GO!");
+
+    document.getElementById("A1").textContent = "";
+    document.getElementById("A2").textContent = "";
+    document.getElementById("A3").textContent = "";
+    document.getElementById("B1").textContent = "";
+    document.getElementById("B2").textContent = "";
+    document.getElementById("B3").textContent = "";
+    document.getElementById("C1").textContent = "";
+    document.getElementById("C2").textContent = "";
+    document.getElementById("C3").textContent = "";
+
+    let divStarting = document.getElementById("startingScreen");
+    let divGameplay = document.getElementById("gameplayScreen");
+    let divButton = document.getElementById("restartButton");
+    divStarting.style.display = "flex";
+    divStarting.style.flexDirection = "column";
+    divStarting.style.alignItems = "center";
+
+    divGameplay.style.display = "none";
+    divButton.style.display = "none";
 }
